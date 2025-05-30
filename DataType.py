321a092acaf9b458ba5691a07173e22b3e1713d6
Saifan62 <saifanzaman681@gamil.com>
2025-05-30 10:01:54 +0600

    Share

---
+++ DataType.py
@@ -0,0 +1,20 @@
+name= "Saifan"
+age = 11
+is_student = True
+height = 4.6
+print('Name: ', name)
+print("Data type of name is", type(name))
+print('Age: ', age)
+print("Data type of age is", type(age))
+print('Student: ', is_student)
+print("Data type of is_student is", type(is_student))
+print('Height: ', height)
+print("Data type of height is", type(height))
+
+print("/n after type casting")
+age= str(age)
+print('Age: ', age)
+print("Data type of age is", type(age))
+height = int(height)
+print('Height: ', height)
+print("Data type of height is", type(height))
