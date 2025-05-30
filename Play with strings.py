edc09718be38e4c7d8638c8553786d7f51b62991
Saifan62 <saifanzaman681@gamil.com>
2025-05-30 18:36:38 +0600

    Play with string.py # This is a simple Python script that demonstrates basic string operations.

---
+++ Play with strings.py
@@ -0,0 +1,14 @@
+first_name= "Saifan"
+last_name = "Zaman"
+full_name = first_name+last_name
+
+print("Full Name: ", full_name)
+print("First Name: ", first_name)
+print("Last Name: ", last_name)
+# Play with strings
+word="Minecraft"
+print("legnth of string :", len(word))
+print("First character of string :", word[0])
+print("Last character of string :", word[5])
+print("First 3 characters of string :", word[:3])
+print("slice of string :", word[2:5])
