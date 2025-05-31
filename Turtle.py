fa083b9071da1b37311fe379bf19d6922a3c7e25
Saifan62 <saifanzaman681@gamil.com>
2025-05-31 10:43:31 +0600

    sd

---
+++ Turtle.py
@@ -0,0 +1,25 @@
+
+
+import turtle
+turtle.Screen().bgcolor('Blue')
+board=turtle.Turtle()
+#Triangle
+board.forward(100)
+board.left(120)
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.goto(50,50)
+#Square
+board.left(30)
+board.pendown()
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
+board.forward(100)
+board.left(90)
