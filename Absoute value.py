56eccbe2bcf0410f144fcc9c0b846aabd17f323e
Saifan62 <saifanzaman681@gamil.com>
2025-06-13 10:19:43 +0600

    gap.py

---
+++ Absoute value.py
@@ -0,0 +1,9 @@
+def absolute_value(num):
+    if num>=0:
+        return num
+    else:
+        return -num
+
+print('Absolute value of 11 is:', absolute_value(11))
+
+print('Absolute value of -15 is:', absolute_value(-15))      
---
+++ Greetfunction.py
@@ -0,0 +1,4 @@
+def greet(name):
+    print("Hello,"+name+", wanna play minecraft?")
+# This function greets the user by name and asks if they want to play Minecraft.
+greet("Existent")
