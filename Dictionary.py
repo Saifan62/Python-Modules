a794703c5991666de3736cef3e1beee7536784c7
Saifan62 <saifanzaman681@gamil.com>
2025-07-08 19:01:58 +0600

    Add Dictionary class for managing student information and attributes

---
+++ Dictionary.py
@@ -0,0 +1,13 @@
+my_dict= {}
+my_dict = {1: 'apple', 2:'ball'}
+my_dict= {'name' : 'Saifan', 1:[2,3,5] }
+my_dict = {'name' : 'Saifan', 'age': 11, 'class': '5th'}
+print('Name:',my_dict['name'])
+print(my_dict['age'])
+print(my_dict['class'])
+my_dict['age'] = 12
+print(my_dict['age'])
+my_dict['school'] = 'Baf Shaheen College'
+print(my_dict['school'])
+my_dict['address'] = 'Bashundhara R/A, Dhaka'
+print(my_dict['address'])
